9916886bbb0f37809f9c6e7ea062055ff7b40b68
SasiRegha <43963426+SasiRegha@users.noreply.github.com>
2020-06-30 18:24:26 +0100

    Update fromLibarary.groovy

--- vars/fromLibarary.groovy
+++ vars/fromLibarary.groovy
@@ -1,4 +1,17 @@
 
-def call() {
+/*def call() {
 	echo "Hello from shared libarary.."
+}*/
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    echo "Hello Vishaal"
+                }
+            }
+        }
+    }
 }
