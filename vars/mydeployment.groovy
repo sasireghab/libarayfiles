00daf51ea274bcaf5a2b2ab29af9f040da3f4a99
SasiRegha <43963426+SasiRegha@users.noreply.github.com>
2020-06-30 17:29:02 +0100

    Create mydeployment.groovy

---
+++ vars/mydeployment.groovy
@@ -0,0 +1,27 @@
+def call(Map pipelineParams) {
+  pipeline
+  {
+    agent any 
+    stages
+    {
+      stage('from jenkinsfile')
+      {
+        steps
+        {
+          echo "from jenkinsfile..."
+          echo "pipelineParams.NAME"
+        }
+      }
+      stage('from lib')
+      {
+        steps
+        {
+          fromLibarary()
+          //fromTestBranch()
+                echo "HELLLLOOO"
+        }
+      }
+
+    }
+  }
+}
