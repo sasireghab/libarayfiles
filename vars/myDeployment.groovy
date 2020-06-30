98b7c61c6fc14fa4a0cea74f0b36bb6d160dfe51
SasiRegha <43963426+SasiRegha@users.noreply.github.com>
2020-06-30 18:23:04 +0100

    Update myDeployment.groovy

--- vars/myDeployment.groovy
+++ vars/myDeployment.groovy
@@ -1,28 +1,13 @@
 def call(Map pipelineParams) {
-  
-  pipeline
-  {
-    agent any 
-    stages
-    {
-      stage('from jenkinsfile')
-      {
-          steps
-          {
-            echo "from jenkinsfile..."
-            echo "pipelineParams.name"
-          }
-      }
-      stage('from lib')
-      {
-          steps
-          {
-            fromLibarary()
-            //fromTestBranch()
-            echo "HELLLLOOO"
-          }
-      }
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    echo "Hello Vishaal"
+                }
+            }
+        }
     }
-  }
-  
 }
