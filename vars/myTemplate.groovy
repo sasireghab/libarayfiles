42295a7edf0a7de162afe73ec4193b709d5544c3
sasireghab <32552815+sasireghab@users.noreply.github.com>
2020-07-03 15:41:45 +0100

    Update myTemplate.groovy

--- vars/myTemplate.groovy
+++ vars/myTemplate.groovy
@@ -24,7 +24,7 @@ def call(body) {
   body.resolveStrategy = Closure.DELEGATE_FIRST
   body.delegate = pipelineParams
   body()
-  echo "pipelineParams ::::::: ${pipelineParams}"
+  echo "pipelineParams ::::::: ${body}"
   
   pipeline{	
     agent any	
