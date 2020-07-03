ddcd386a8d668e854cc0066fd33a4039d8cc9e27
sasireghab <32552815+sasireghab@users.noreply.github.com>
2020-07-03 15:21:18 +0100

    Update myTemplate.groovy

--- vars/myTemplate.groovy
+++ vars/myTemplate.groovy
@@ -1,4 +1,30 @@
-def call(Map pipelineParams) {
+/*def call(Map pipelineParams) {
+  pipeline{	
+    agent any	
+      stages	{		
+          stage('from jenkinsfile')		{			
+              steps			{				
+                  echo "Hiiiiiii....."	
+                }		
+           }		
+           stage('from lib')		{	
+              environment {
+                    def name = "${pipelineParams.MY_NAME}" 
+                }
+              steps			{				
+                  fromLibarary()
+                  echo "MY_NAME :: $name"
+               }		
+            }		
+         }
+      }
+}*/
+def call(body) {
+  def pipelineParams = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+  
   pipeline{	
     agent any	
       stages	{		
